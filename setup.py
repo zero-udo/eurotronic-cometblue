85b80404ec5a4a1e751a93b32632cd1c3e81ff35
Johannes Rottler <johannes@rottler.me>
2020-09-08 15:04:08 +0200

    updated setup.py - first release Version 1.0.0

--- setup.py
+++ setup.py
@@ -8,20 +8,18 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="eurotronic-cometblue",
-    version="0.1.0",
+    version="1.0.0",
     author="Johanens Rottler",
     author_email="johannes@rottler.me",
     description="Allows you to access Eurotronics GmbH BLE Comet Blue Radiator Controller",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/zero-udo/CometBlue",
+    url="https://github.com/zero-udo/eurotronic-cometblue",
     packages=setuptools.find_packages(),
     license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows :: Windows 10",
         "Operating System :: POSIX :: Linux",
         "Development Status :: 5 - Production/Stable",
     ],
