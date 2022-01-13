02bff9a967a7d379cd9a93c39d4cc2077c554046
Johannes Rottler <johannes@rottler.me>
2022-01-13 17:13:14 +0100

    bump version to 1.0.3 due to issue while building/packaging

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="eurotronic-cometblue",
-    version="1.0.2",
+    version="1.0.3",
     author="Johanens Rottler",
     author_email="johannes@rottler.me",
     description="Allows you to access Eurotronic GmbH BLE Comet Blue Radiator Controller",
